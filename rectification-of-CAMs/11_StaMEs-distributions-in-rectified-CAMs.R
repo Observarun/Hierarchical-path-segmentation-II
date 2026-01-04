f7a5623887b0d605af715d9cdd05ec9df42aeca2
Varun Sethi <83636458+Observarun@users.noreply.github.com>
2026-01-04 04:45:31 -0600

    bug fix

--- rectification-of-CAMs/11_StaMEs-distributions-in-rectified-CAMs.R
+++ rectification-of-CAMs/11_StaMEs-distributions-in-rectified-CAMs.R
@@ -14,7 +14,7 @@ for(c in 1:k){
   words_in_CAMs[[c]] <- 
     append(
       words_in_CAMs[[c]],
-      rep(df_labels_counts_maxcountcluster$distinct_labels [which(df_labels_counts_maxcountcluster$max_count_cluster %in% c)],
+      rep(df_labels_counts_maxcountcluster$distinct_labels[which(df_labels_counts_maxcountcluster$max_count_cluster %in% c)],
           df_labels_counts_maxcountcluster$total_count[which(df_labels_counts_maxcountcluster$max_count_cluster %in% c)])
     )
 }
