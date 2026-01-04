d393b769e4adc1eb344f55f0af8faf2328c707ce
Varun Sethi <83636458+Observarun@users.noreply.github.com>
2026-01-03 20:27:29 -0600

    minor debugging

--- clustering/01_data-preparation_barn-owl.R
+++ clustering/01_data-preparation_barn-owl.R
@@ -23,7 +23,7 @@ for (i in nbrs_DAR_indiv){
 
 df_DARs_indiv <- df_DARs_indiv[, c('X','Y','dateTime')]
 colnames(df_DARs_indiv)[1] <- "x"
-colnames(df_DARs_indiv)[y] <- "y"
+colnames(df_DARs_indiv)[2] <- "y"
 colnames(df_DARs_indiv)[3] <- "t"
 
 df_DARs_indiv <- transform(df_DARs_indiv, t=as.numeric(t))
@@ -54,14 +54,14 @@ turning_angle <-
 turning_angle <-
   with(
     df_DARs_indiv,
-    angle_turning%%(2*pi)
+    turning_angle%%(2*pi)
   )
 turning_angle <-
   with(
     df_DARs_indiv,
     ifelse(turning_angle>pi, turning_angle-2*pi, turning_angle)
   )
-#angle_turning <- abs(angle_turning)  # only for absolute (or reflection invariant) turning angle; to work with actual value of turning angle, line 66 must be commented
+#turning_angle <- abs(turning_angle)  # only for absolute (or reflection invariant) turning angle; to work with actual value of turning angle, line 66 must be commented
 df_DARs_indiv <- df_DARs_indiv[-1, ]  # first point discarded for the lack of turning angle value
 df_DARs_indiv$heading <- turning_angle
 colnames(df_DARs_indiv)[5] <- 'turning_angle'
@@ -77,7 +77,7 @@ df_DARs_indiv <-
 
 
 df_DARs_indiv$speed <-
-  (df_DARs_indiv$speed)/max(df_DARs$speed)
+  (df_DARs_indiv$speed)/max(df_DARs_indiv$speed)
 df_DARs_indiv$turning_angle <-
   (df_DARs_indiv$turning_angle)/pi
 # Scaling to bring the variables to range on [0,1].
